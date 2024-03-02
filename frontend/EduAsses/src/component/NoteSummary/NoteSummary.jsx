025f990ee937a30f86a680f5de5accfe20e3912e
Anuj Dagade <dagadeanuj@gmail.com>
2024-03-02 16:54:27 +0000

    Refactor App and NoteSummary components

--- frontend/EduAsses/src/component/NoteSummary/NoteSummary.jsx
+++ frontend/EduAsses/src/component/NoteSummary/NoteSummary.jsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react"
+
 export function NoteSummary() {
+    const [selectedFile, setSelectedFile ] = useState()
+
+    function onFileChange(event) {
+        setSelectedFile(event.target.files[0])
+    }
+
+    function onFileUpload(event) {
+        event.preventDefault();
+        const formData = new FormData()
+        formData.append("file", selectedFile)
+
+        console.log(formData);
+
+        //api call to upload file
+    }
+
     return (
         <div>
             <form >
                 <label htmlFor="file"></label>
+                <input onChange={onFileChange} type="file" id="file" name="file" />
+                <button onClick={onFileUpload}>Upload</button>
             </form>
         </div>
     )
