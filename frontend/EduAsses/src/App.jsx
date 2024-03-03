19239e0d0b3e5271d532d07f3686287acb4e3273
Anuj Dagade <dagadeanuj@gmail.com>
2024-03-03 14:36:50 +0000

    Add NavBar component and Test route

--- frontend/EduAsses/src/App.jsx
+++ frontend/EduAsses/src/App.jsx
@@ -5,7 +5,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NoteSummary from './component/NoteSummary/NoteSummary'
 import Login from './component/Login/Login'
 import Home from './component/Home';
-
+import NavBar from './component/NavBar/NavBar'
+import Test from './component/Grading/Test'
 
 function App() {
 
@@ -15,10 +16,13 @@ function App() {
 
       
       <Router >
+      <NavBar />
         <Routes>
           <Route path='/NoteSummary' element={<NoteSummary />} />
           <Route path='/Login' element={<Login />} />
+          <Route path='/Test' element={<Test />} />
           <Route path='/' element={<Home />} />
+           {/* <Route path='/NoteSummary' element={<NavBar />} /> */}
         </Routes>
       </Router>
     </>
